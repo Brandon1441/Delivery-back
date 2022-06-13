c0a84376f3f9c4b99b51c53c09d082a3f4d1191f
BrandonMunozUTN <brandon.munoz@alumnos.frm.utn.edu.ar>
2022-06-13 13:25:34 -0300

    first commit

---
+++ src/routes/products.routes.js
@@ -0,0 +1,47 @@
+import { Router } from "express";
+import {
+  createNewUser,
+  deleteUserByPass,
+  getUser,
+  getUserByPass,
+
+  getStore,
+  createNewStore,
+  getStoreById,
+  deleteStoreById,
+  
+  getClient,
+  createNewClient,
+  getClientById,
+  deleteClientById,
+  
+  createNewSale,
+  getSaleById,
+  getShipment_0
+} from "../controllers/products.controller.js";
+
+const router = Router();
+
+router.get("/users", getUser);
+router.post("/users", createNewUser);
+router.get("/users/:pass/:user", getUserByPass);
+router.delete("/users/:pass/:user", deleteUserByPass);
+
+router.get("/stores", getStore);
+router.post("/stores", createNewStore);
+router.get("/stores/:id", getStoreById);
+router.delete("/stores/:id", deleteStoreById);
+
+router.get("/clients", getClient);
+router.post("/clients", createNewClient);
+router.get("/clients/:id", getClientById);
+router.delete("/clients/:id", deleteClientById);
+
+router.get("/sales/:codUser", getSaleById);
+router.post("/sales", createNewSale);
+
+router.get("/shipments", getShipment_0);
+
+
+
+export default router;
